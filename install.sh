4b6d6480dc6a68c86c05f385f14ee204af9603a0
Pavel Nosovets <nosovets@scand.com>
2018-06-19 14:48:02 +0300

    Added sudo to ag installation

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Ag 
 echoHeader 'Installing Silver Searcher'
-apt-get install silversearcher-ag
+sudo apt-get install -y silversearcher-ag
 
 # Update VIM to v8
 echoHeader 'Installing VIM8'
