8bfd6ad1b228199d23e21465108d02a35ec7e19e
Pavel Nosovets <nosovets@scand.com>
2018-06-29 11:51:32 +0300

    Minor fix

--- install.sh
+++ install.sh
@@ -40,14 +40,14 @@ vim +slient +PluginInstall +qall </dev/tty
 
 # .bashrc
 echoHeader 'Updating .bashrc'
-echo 'FZF_CTRL_T_COMMAND=`ag --hidden --ignore .git -g ""`' | sed "s/\`/'/" >> ~/.bashrc
-echo 'FZF_DEFAULT_COMMAND=`ag --hidden --ignore .git -g ""`' | sed "s/\`/'/"  >> ~/.bashrc
+echo 'FZF_CTRL_T_COMMAND=`ag --hidden --ignore .git -g ""`' | sed "s/\`/'/g" >> ~/.bashrc
+echo 'FZF_DEFAULT_COMMAND=`ag --hidden --ignore .git -g ""`' | sed "s/\`/'/g"  >> ~/.bashrc
 echo 'export FZF_DEFAULT_OPTS="
   --color fg:188,bg:233,hl:103,fg+:222,bg+:234,hl+:104
   --color info:183,prompt:110,spinner:107,pointer:167,marker:215
         --color=marker:#719e07,fg+:#839496,prompt:#719e07,hl+:#719e07
-       "' | sed "s/\"/'/" >> ~/.bashrc
-echo 'export FZF_CTRL_T_OPTS="-m --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all"' | sed "s/\"/'/" >> ~/.bashrc
+       "' | sed "s/\"/'/g" >> ~/.bashrc
+echo 'export FZF_CTRL_T_OPTS="-m --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all"' | sed "s/\"/'/g" >> ~/.bashrc
 
 # Aliases
 echoHeader 'Adding bash aliases'
